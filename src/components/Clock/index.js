f14cb19e6efed009c375ec478520ebcee22cdba2
vijy0123 <vijaydyagala0123@gmail.com>
2023-06-06 06:05:35 +0000

    Clock Application Schedulers

---
+++ src/components/Clock/index.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from 'react'
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    // console.log(timerId)
+    console.log('Effect executed and Interval is schedule')
+    return () => {
+      clearInterval(timerId)
+      console.log('Cleanup Function executed')
+    }
+  }, [])
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time>{date.toLocaleTimeString()}</Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
